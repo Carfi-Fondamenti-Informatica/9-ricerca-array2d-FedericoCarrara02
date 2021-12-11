ef0588a7efd6f9fd753f3afc8520b64514de183c
FedericoCarrara02 <91536069+FedericoCarrara02@users.noreply.github.com>
2021-12-11 18:49:00 +0100

    Update lib.h

--- lib.h
+++ lib.h
@@ -1,1 +1,6 @@
+#ifndef ESERCITAZIONE_VALUTATA_9_LIB_H
+#define ESERCITAZIONE_VALUTATA_9_LIB_H
 
+int posizione_match (char lista_nomi[10][20], char nome[]);
+
+#endif //ESERCITAZIONE_VALUTATA_9_LIB_H
